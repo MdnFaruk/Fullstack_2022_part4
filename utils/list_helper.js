26d3b387ead500e7197a016d61358a609384ec62
Md Nuruzzaman Faruk <mdnfaruk.fin@gmail.com>
2022-03-23 20:44:40 +0200

    4.3-4.7 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,34 @@
-const dummy = (blogs) => {
+const dummy = blogs => {
     return 1
   }
-  
-  module.exports = {
-    dummy
-  }
+
+const totalLikes = blogs => blogs.reduce((t,{likes}) => t + likes,0)
+
+const favoriteBlog = blogs => {
+  const maxLike = Math.max.apply(null,Object.values(blogs).map(element => element.likes))
+  const {_id,__v,url,...rest} = blogs.find(element => element.likes === maxLike)
+  return rest
+}
+
+const mostBlogs = blogs => {
+  const authors = {};
+  const authorList = Object.values(blogs).map(b => b.author)
+  authorList.forEach(element => { authors[element] = (authors[element] || 0) + 1});
+  const maxBlog = Math.max(...Object.values(authors))
+  const maxBlogAuthor = Object.entries(authors).filter(element => element[1] === maxBlog)[0]
+  return {author:maxBlogAuthor[0],blogs:maxBlogAuthor[1]}
+}
+
+const mostLikes = blogs => {
+  const maxLike = Math.max.apply(null,Object.values(blogs).map(element => element.likes))
+  const {_id,__v,url,title,...rest} = blogs.find(element => element.likes === maxLike)
+  return rest
+}
+
+module.exports = {
+  dummy, 
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
